c74a992226cd93fbffbacad37cb12aa923830f8f
DSA <dsa@Anirudhs-MacBook-Air.local>
2025-10-07 22:41:00 +0530

    Initial frontend

---
+++ client/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind + React!</h1>
+    </div>
+  );
+}
+
+
+export default App
